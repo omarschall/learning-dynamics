8b2c7e5607473f8d9725be94f49fb3071b95d4a9
Owen Marschall <oem214@log-3.nyu.cluster>
2021-06-08 17:39:00 -0400

    First commit

---
+++ cluster_main_scripts/analyze.py
@@ -0,0 +1,1 @@
+#ok we do it here
---
+++ cluster_main_scripts/pipeline_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+sys.path.append('/scratch/oem214/vanilla-rtrl/')
+from cluster import submit_jobs
+from continual_learning import *
+from core import *
+from dynamics import *
+from functions import *
+from gen_data import *
+from learning_algorithms import *
+from optimizers import *
+from plotting import *
+from wrappers import *
+
+
+import numpy as np
+print(np.pi)
+
+
