b2aa2c07ee1d842980093e0a551357d4445167d3
Owen Marschall <oem214@log-3.nyu.cluster>
2021-06-08 18:15:14 -0400

    More jupyter notebook changes

---
+++ jupyter_notebook/jupyter.s
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16GB
+#SBATCH --time=24:00:00
+#SBATCH --job-name=jupyter
+#SBATCH --output=/home/oem214/jupyter_notebook/jupyter.o
+
+unset XDG_RUNTIME_DIR
+if [ "$SLURM_JOBTMP" != "" ]; then
+    export XDG_RUNTIME_DIR=$SLURM_JOBTMP
+fi
+
+module purge
+
+port=$(shuf -i 10000-65500 -n 1)
+
+/usr/bin/ssh -N -f -R $port:localhost:$port log-1
+/usr/bin/ssh -N -f -R $port:localhost:$port log-2
+/usr/bin/ssh -N -f -R $port:localhost:$port log-3
+
+cat<<EOF
+
+Jupyter server is running on: $(hostname)
+Job starts at: $(date)
+
+Step 1 :
+
+If you are working in NYU campus, please open an iTerm window, run command
+
+ssh -L $port:localhost:$port $USER@greene.hpc.nyu.edu
+
+If you are working off campus, you should already have ssh tunneling setup through HPC bastion host, 
+that you can directly login to greene with command
+
+ssh $USER@greene
+
+Please open an iTerm window, run command
+
+ssh -L $port:localhost:$port $USER@greene
+
+Step 2:
+
+Keep the iTerm windows in the previouse step open. Now open browser, find the line with
+
+The Jupyter Notebook is running at: $(hostname)
+
+the URL is something: http://localhost:${port}/?token=XXXXXXXX (see your token below)
+
+you should be able to connect to jupyter notebook running remotly on greene compute node with above url
+
+EOF
+
+cd /scratch/oem214/learning-dynamics/notebooks/
+singularity exec --nv --overlay /scratch/oem214/vanilla-rtrl/pytorch1.7.0-cuda11.0.ext3:ro \
+/scratch/work/public/singularity/cuda11.0-cudnn8-devel-ubuntu18.04.sif \
+bash -c "source /ext3/env.sh; jupyter notebook --no-browser --port $port --notebook-dir=$(pwd); exit"
+
