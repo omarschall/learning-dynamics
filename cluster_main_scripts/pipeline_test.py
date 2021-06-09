067a219ce67dcc7ccedf03a0efc9f4a714c7ef3d
Owen Marschall <oem214@log-2.nyu.cluster>
2021-06-09 17:54:18 -0400

    Pre refactor commit

--- cluster_main_scripts/pipeline_test.py
+++ cluster_main_scripts/pipeline_test.py
@@ -15,7 +15,25 @@ from plotting import *
 from wrappers import *
 
 
-import numpy as np
-print(np.pi)
+### --- Set up all configs --- ###
+from itertools import product
+n_seeds = 10
+macro_configs = config_generator(param_1=[1, 2],
+                                 param_2=[10, 100])
+micro_configs = tuple(product(macro_configs, list(range(n_seeds))))
+
+### --- Select particular config --- ###
+try:
+    i_job = int(os.environ['SLURM_ARRAY_TASK_ID']) - 1
+except KeyError:
+    i_job = 0
+params, i_seed = micro_configs[i_job]
+i_config = i_job//n_seeds
+
+new_random_seed_per_condition = True
+if new_random_seed_per_condition:
+    np.random.seed(i_job)
+else: #Match random seeds across conditions
+    np.random.seed(i_seed)
 
 
